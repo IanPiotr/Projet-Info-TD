bd795adbe7470b504b8c37773690b1bd0be6319e
InsaBob <ruben.pericas-moya@insa-lyon.fr>
2015-05-21 15:14:21 +0200

    Quick màj3
    Ca à l'air de buguer quand le chemin est tout à fait sur le bord de
    l'écran.

--- Fenetre.java
+++ Fenetre.java
@@ -632,47 +632,7 @@ public class Fenetre extends JFrame{
     }
 	
 	/* METHODES DE DEBUGAGE */
-	
-	/* Visualisation bordures */
-	private void showBordures(Graphics gb){
-		Case curc = chemin.bordureBas.root;
-		while(curc != null){
-			gb.setColor(Color.green);
-			if(curc.hybride){
-				gb.setColor(Color.blue);
-			}
-			curc.drawCase(gb);
-			curc = curc.next;				
-		}
-		curc = chemin.bordureHaut.root;
-		while(curc != null){
-			gb.setColor(Color.pink);
-			if(curc.hybride){
-				gb.setColor(Color.blue);
-			}
-			curc.drawCase(gb);
-			curc = curc.next;
-		}
-		curc = chemin.bordureGauche.root;
-		while(curc != null){
-			gb.setColor(Color.orange);
-			if(curc.hybride){
-				gb.setColor(Color.blue);
-			}
-			curc.drawCase(gb);
-			curc = curc.next;
-		}
-		curc = chemin.bordureDroite.root;
-		while(curc != null){
-			gb.setColor(Color.red);
-			if(curc.hybride){
-				gb.setColor(Color.blue);
-			}
-			curc.drawCase(gb);
-			curc = curc.next;
-		}
-	}
-	
+
 	/* Visualisation Rectangle + Arc2D entourant les ennemis */
 	private void showContoursEnnemi(Graphics gb, Ennemis bob){
 		gb.setColor(Color.red);
